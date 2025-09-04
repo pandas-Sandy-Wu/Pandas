fab5c0422715a463716f6be2feb717c7e3a4f23a
Ashley Wu <ashley.wu@stonybrook.edu>
2025-09-04 00:03:51 -0400

    Create initial frontend setup files

---
+++ frontend/web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>This is the Frontend (under construction with Typescript)</h1>
+      <p>Nothing to see for now...</p>
+      <p>Insert file uploading service</p>
+    </div>
+  );
+}
